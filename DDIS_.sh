d1e274641fa1b79037d2f1c0a0e5acd3ce0801a7
Rina Tagami <92970522+tagamirina@users.noreply.github.com>
2023-01-18 15:44:28 +0900

    Delete DDIS_.sh

--- DDIS_.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for ((i=1 ; i<=105 ; i+=2))
-do
-	./DDIS -tmp ./data/${i}.jpg -i ./data/$((i+1)).jpg -txt ./data/${i}.txt -res ./outputIMG/output${i}.png -log ./outputTXT/output${i}.txt -RectDDIS ./outputIMG/heatmap${i}.png -v 1 -mode 0
-
-done
